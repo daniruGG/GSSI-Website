e02570be7c275a08e915d708951fa884a1d6b985
daniruGG <danniru00@gmail.com>
2023-03-18 13:44:12 +0800

    fixed the footer for each products

--- products-folder/small-large-food-waste-strainer.php
+++ products-folder/small-large-food-waste-strainer.php
@@ -62,57 +62,8 @@
     </div>
 
     <!-- FOOTER -->
-    <div class="footer-basic">
-        <footer>
-            <div class="row mx-0">
-                <div class="col-sm my-auto">
-                    <img src="../img/logo2.png" class="mx-auto" alt="img" style="width: 150px;">
-                </div>
-                <div class="col-sm mt-5">
-                    <ul class="list-inline">
-                        <li class="list-inline-item"><a href="../index.php">Home</a></li>
-                        <li class="list-inline-item"><a href="../about.php">About</a></li>
-                        <li class="list-inline-item"><a href="../product.php">Products</a></li>
-                        <li class="list-inline-item"><a href="../service.php">Services</a></li>
-                        <li class="list-inline-item"><a href="../project.php">Projects</a></li>
-                        <li class="list-inline-item"><a href="../contact.php">Contact</a></li>
-                    </ul>
-                    <div class="social">
-                        <a href="https://www.facebook.com/profile.php?id=100064865100649" target="_blank">
-                            <ion-icon name="logo-facebook"></ion-icon>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-sm mt-5">
-                    <div class="row">
-                        <div class="col-sm">
-                            <div class="d-flex mb-1">
-                                <i class="fa fa-envelope pe-2 text-warning pt-1"></i>
-                                <p class="text-white">gssi_enterprise@yahoo.com</p>
-                            </div>
-                            <div class="d-flex mb-1">
-                                <i class="fa fa-fax pe-2 text-warning pt-1"></i>
-                                <p class="text-white">(02) 8395 4623 / (02) 8293 8026</p>
-                            </div>
-                            <div class="d-flex mb-1">
-                                <i class="fa fa-mobile pe-2 text-warning fs-4"></i>
-                                <p class="text-white">(0926) 752 6438 / (0966) 870 0588 / (0991) 843 0220</p>
-                            </div>
-                            <div class="d-flex mb-1">
-                                <i class="fa fa-map-marker pe-2 text-warning fs-5"></i>
-                                <p class="text-white">Block 17, Lot 12 Armal Village, Sta.Ana, Taytay, Rizal</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row m-0">
-                <div class="col text-center">
-                    <p class="copyright">© GSSI Enterprise 2023</p>
-                </div>
-            </div>
-        </footer>
-    </div> <!-- END FOOTER -->
+    <?php include_once("products-footer.php"); ?>
+    <!-- END FOOTER -->
 
     <!-- ION ICONS -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
